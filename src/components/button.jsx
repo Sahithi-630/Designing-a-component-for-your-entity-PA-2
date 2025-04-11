dcdb6b07d413ae48bc5c2f427dffca6a07da3f96
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-04-11 13:33:05 +0530

    PA-2

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,22 @@
 // write button card here
+import React from "react";
+
+const ViewProductButton = () => {
+  return (
+    <button style={styles.button}>View Product</button>
+  );
+};
+
+const styles = {
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#007BFF",
+    color: "#fff",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,48 @@
 // write product card here
+import React from "react";
+import ViewProductButton from "../components/button";
+
+const Productcard = () => {
+  // Static details
+  const productImage = "https://m.media-amazon.com/images/I/71UkRZji9fL.jpg";
+  const productName = "Kawasaki";
+  const productPrice = "$20.99";
+
+  return (
+    <div style={styles.card}>
+      <img src={productImage} alt={productName} style={styles.image} />
+      <h2 style={styles.name}>{productName}</h2>
+      <p style={styles.price}>{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+const styles = {
+  card: {
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "16px",
+    textAlign: "center",
+    width: "250px",
+    boxShadow: "0 4px 8px rgba(94, 92, 92, 0.1)",
+  },
+  image: {
+    width: "100%",
+    height: "150px",
+    objectFit: "cover",
+    borderRadius: "8px 8px 0 0",
+  },
+  name: {
+    fontSize: "18px",
+    margin: "12px 0 6px",
+    color: "white",
+  },
+  price: {
+    fontSize: "16px",
+    margin: "6px 0",
+    color: "grey",
+  },
+};
+
+export default Productcard;
